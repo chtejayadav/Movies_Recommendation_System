d250a2ca82780115bfd1432c35919bbdce1993e1
Ganugapenta Anil <anilanilg457@gmail.com>
2025-03-19 14:16:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-import joblib
+import pickle
 import pandas as pd
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
